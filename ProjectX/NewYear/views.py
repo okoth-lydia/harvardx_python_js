1b2db18f3d71ccd5afb5e4828a21d954a2666eb3
okoth-lydia <lydiahokoth13@gmail.com>
2021-02-12 13:08:27 +0300

    add python logic to html

--- ProjectX/NewYear/views.py
+++ ProjectX/NewYear/views.py
@@ -1,3 +1,12 @@
+#import the datetime module
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    #create a variable that accesses datetime now functionality
+    now = datetime.datetime.now()
+    return render(request, "NewYear/index.html", {
+        "NewYear": now.month == 1 and now.day == 1
+    })
